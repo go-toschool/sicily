6798d82129931809abbb32a2c1c14357b89ffa39
Rodrigo Fuenzalida <rfuenzalida@acid.cl>
2018-12-25 16:07:36 -0300

    context and handlers

---
+++ cmd/server/session/context.go
@@ -0,0 +1,62 @@
+package session
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-toschool/palermo/auth"
+	"github.com/go-toschool/syracuse/citizens"
+	"github.com/graphql-go/graphql"
+)
+
+// ContentTypeGraphQL graphql content type.
+const (
+	ContentTypeGraphQL   = "application/graphql"
+	authUserIDContextKey = "fromTokenSessionUserId"
+	tokenTypePrefix      = "Bearer "
+	tokenHeaderKey       = "Authorization"
+	tokenMetaKey         = "auth_token"
+)
+
+// GraphRequest struct to unmarshal query.
+type GraphRequest struct {
+	Query string `json:"query"`
+}
+
+// Context ...
+type Context struct {
+	User    citizens.CitizenshipClient
+	Session auth.AuthServiceClient
+	Schema  graphql.Schema
+}
+
+// Handle creates a new bounded Handler with context.
+func (c *Context) Handle(h HandlerFunc) *Handler {
+	return &Handler{c, h}
+}
+
+// ExecuteQuery ...
+func (c *Context) ExecuteQuery(query string) *graphql.Result {
+	result := graphql.Do(graphql.Params{
+		Schema:        c.Schema,
+		RequestString: query,
+	})
+	if len(result.Errors) > 0 {
+		fmt.Printf("errors: %v\n", result.Errors)
+	}
+
+	return result
+}
+
+// HandlerFunc function handler signature used by sigiriya application.
+type HandlerFunc func(*Context, http.ResponseWriter, *http.Request)
+
+// Handler ...
+type Handler struct {
+	ctx    *Context
+	handle HandlerFunc
+}
+
+func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	h.handle(h.ctx, w, r)
+}
---
+++ cmd/server/users/users.go
@@ -0,0 +1,55 @@
+package users
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"strings"
+
+	"github.com/go-toschool/sicily"
+	"github.com/graphql-go/graphql"
+)
+
+// Users ...
+func Users(ctx *Context, w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "This server does not support that HTTP method", http.StatusBadRequest)
+		return
+	}
+
+	id, ok := r.Context().Value(sicily.AuthUserIDContextKey).(string)
+	if !ok {
+		http.Error(w, "missing user id", http.StatusBadRequest)
+		return
+	}
+
+	contentTypeStr := r.Header.Get("Content-Type")
+	contentTypeTokens := strings.Split(contentTypeStr, ";")
+	contentType := contentTypeTokens[0]
+
+	var result *graphql.Result
+	switch contentType {
+	case sicily.ContentTypeGraphQL:
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "could not read body", http.StatusInternalServerError)
+			return
+		}
+
+		gr := &GraphRequest{}
+
+		if err := json.Unmarshal(body, gr); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		result = ctx.ExecuteQuery(gr.Query, id)
+	default:
+		http.Error(w, "bad content type", http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Accept-Encoding", "gzip")
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(result)
+}
