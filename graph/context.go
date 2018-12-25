ec5a093e488be5c1c24fbb55dc8ad5a51084249c
Rodrigo Fuenzalida <rfuenzalida@acid.cl>
2018-12-25 16:07:53 -0300

    add grpc clients

--- graph/context.go
+++ graph/context.go
@@ -1,7 +1,13 @@
 package graph
 
-import "github.com/go-toschool/syracuse/citizens"
+import (
+	"github.com/go-toschool/palermo/auth"
+	"github.com/go-toschool/platon/talks"
+	"github.com/go-toschool/syracuse/citizens"
+)
 
 type Context struct {
-	UserService citizens.CitizenshipClient
+	UserService    citizens.CitizenshipClient
+	TalkService    talks.TalkingClient
+	SessionService auth.AuthServiceClient
 }
