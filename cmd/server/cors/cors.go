11aeaf1aba4c912da16e2fffbf58a967cc05644e
Rodrigo Fuenzalida <rfuenzalidac87@gmail.com>
2019-01-26 18:00:19 -0300

    cors: check cors compatibility

---
+++ cmd/server/cors/cors.go
@@ -0,0 +1,26 @@
+package cors
+
+import "net/http"
+
+type AuthCors struct {
+}
+
+// Check ...
+func (ac *AuthCors) Check(next http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Authorization, Content-Type, Content-Length, Accept-Encoding")
+		if r.Method == "OPTIONS" {
+			w.Header().Set("Access-Control-Max-Age", "86400")
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	}
+}
+
+func NewCors() *AuthCors {
+	return &AuthCors{}
+}
